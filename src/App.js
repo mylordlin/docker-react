6c50b3a7e27867bacec8a9875efab1fa7a68b685
mylordlin <bfcatfriends@gmail.com>
2020-02-10 14:15:18 +1100

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hi, to get started, edit <code>src/App.js</code> and save to reload.
+          Hi, to get started on the feature branch, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
